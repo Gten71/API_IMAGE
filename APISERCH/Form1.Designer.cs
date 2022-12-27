cb815d6f573dd9a3a98514f62f852f36f306372b
Gten71 <mresnyansky@gmail.com>
2022-12-27 13:31:24 +0300

    add on github

---
+++ APISERCH/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace APISERCH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(448, 9);
+            this.buttonSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(121, 25);
+            this.buttonSearch.TabIndex = 0;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(9, 10);
+            this.textBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(429, 20);
+            this.textBox.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 47);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.MinimumSize = new System.Drawing.Size(100, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(791, 440);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 522);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.buttonSearch);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ APISERCH/Form1.cs
@@ -0,0 +1,74 @@
+﻿using AngleSharp.Html.Parser;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Windows.Forms;
+
+namespace APISERCH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            string templateUrl = "https://www.bing.com/images/search?q=" + textBox.Text;
+            
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                MessageBox.Show("Please supply a search term"); return;
+            }
+            else
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    wc.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)");
+                    string result = wc.DownloadString(templateUrl);
+                   
+                        //HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                        //doc.LoadHtml(result);
+                        
+                        //var Img = from a in doc.DocumentNode.Descendants("img")
+                        //          from img in a.Descendants("img")
+
+                        //              // where a.Attributes["class"] != null && a.Attributes["class"].Value == "img"
+                        //              //&& img.Attributes["src"] != null && img.Attributes["src"].Value.Contains("img")
+                        //          select img;
+                        
+                        var parser = new HtmlParser();
+                        var document = parser.ParseDocument(result);
+                        var elementsOl = document.GetElementsByTagName("img");
+                        var el=elementsOl[1];
+                    
+                        string txt = el.OuterHtml;
+                        var doc = parser.ParseDocument(txt);
+                        var href = doc.QuerySelector("img");
+                        string url = href.GetAttribute("src");
+
+                        byte[] downloadedData = wc.DownloadData(url);
+
+
+                        if (downloadedData != null)
+                        {
+                            System.IO.MemoryStream ms = new System.IO.MemoryStream(downloadedData, 0, downloadedData.Length);
+                            ms.Write(downloadedData, 0, downloadedData.Length);
+                            pictureBox1.Image = Image.FromStream(ms);
+                        }
+
+                    }
+
+                
+            }
+        }
+    }
+}
